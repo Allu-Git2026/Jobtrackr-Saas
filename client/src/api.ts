dbed6c8550c21775bf66cc531fceabfd06593554
Chaitu-Works <chaituallu2025@gmail.com>
2026-03-04 12:31:34 -0600

    frontend deployment config

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001",
+  withCredentials: true
 });
 
 api.interceptors.request.use((config) => {
